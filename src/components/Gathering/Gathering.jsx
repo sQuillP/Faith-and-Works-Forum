ab85cb0b7a1bcee103bcb48edaa933ff9ec27d84
unknown <willpattisons@yahoo.com>
2024-05-25 17:51:49 -0400

    fixed typography layout

--- src/components/Gathering/Gathering.jsx
+++ src/components/Gathering/Gathering.jsx
@@ -3,7 +3,7 @@ import { useContext, useEffect, useMemo, useState } from "react"
 import Navbar from "../_global/Navbar";
 import Footer from "../_global/Footer";
 import PageTitle from "../_global/PageTitle";
-import { Stack, Button, Typography, Box } from "@mui/material";
+import { Stack, Button, Typography, Box, useMediaQuery } from "@mui/material";
 import GatheringCard from "./GatheringCard";
 import GatheringForm from "./GatheringForm";
 import { useNavigate } from "react-router-dom";
@@ -18,7 +18,7 @@ export default function Gathering() {
     const [dt, setDT] = useState(getFormattedDate());
     const navigate = useNavigate();
 
-
+    const smallscreen = useMediaQuery('(max-width:600px)');
 
     function _getDateAndTime() {
         // ex: "Tuesday, May 14 from 7 - 9pm EDT"
@@ -27,9 +27,6 @@ export default function Gathering() {
             return;
         }
 
-        if(new Date() > new Date(gathering[0])) {
-            
-        }
         const options = {
             weekday: 'long',
             year: 'numeric',
@@ -88,10 +85,10 @@ export default function Gathering() {
                 <div className="gathering-main">
                 {
                     gathering.length === 0 || new Date() > gatheringDate ? (
-                        <Box m={14}>
-                            <Typography variant="h3" fontFamily={'inherit'} textAlign={'center'}>Our next gathering date is to be determined.</Typography>
+                        <Box width={'100%'} display={'flex'} justifyContent={'center'} alignItems={'center'} flexDirection={"column"} mt={14}>
+                            <Typography fontSize={smallscreen ? '25px':'40px'} variant="p" fontFamily={'inherit'} textAlign={'center'}>Our next gathering date is to be determined.</Typography>
                             {/* <Typography variant="h5" m={3} fontFamily={'inherit'} textAlign={'center'}>Stay up to date with all we're doing by subscribing to our email list!</Typography> */}
-                            <Typography variant="h5" margin={'30px auto'} maxWidth={'900px'} fontFamily={'inherit'} textAlign={'center'}>
+                            <Typography fontSize={smallscreen ? '16px' : '30px'} variant="p" width={smallscreen ? '100%':'unset'} margin={'30px auto'} maxWidth={'900px'} fontFamily={'inherit'} textAlign={'center'}>
                                 Stay connected with our forum as planning is currently in the works!
                                 We will be announcing our next gathering as soon as plans are underway.
                             </Typography>
